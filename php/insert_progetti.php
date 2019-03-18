1ad4a0b7f7dcf9c806b76d3b68a4bfc457139f6d
Federico Bolcato <2306632+Freedye@users.noreply.github.com>
2019-03-18 21:42:22 +0100

    server connection fix
    Credo sia così bho Ginoz aiuto

--- php/insert_progetti.php
+++ php/insert_progetti.php
@@ -1,6 +1,6 @@
 <?php
     function getAllProjects(){
-        $connection = mysqli_connect("serverName", "User", "", "psw");
+        $connection = mysqli_connect("serverName", "user", "psw", "tableName");
         $rsProjects = mysqli_query($connection, "SELECT * FROM projects");
         $projects = mysqli_fetch_all($rsProjects, MYSQLI_ASSOC);
         mysqli_close($connection);
