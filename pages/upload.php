e51816f0b3b7eab77be7122428276e8fb6f17f78
mginetti <matteo.ginetti99@gmail.com>
2019-03-22 11:10:17 +0100

    Modifica upload.php

--- pages/upload.php
+++ pages/upload.php
@@ -8,34 +8,36 @@
     <link href="../css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="../js/bootstrap.min.js"></script>
     <script src="../js/jquery-3.3.1.min.js"></script>
+    <script src="../js/script.js"></script>
 
-    <title>Document</title>
+    <title>Upload</title>
 </head>
 <body>
     <header>
-    <nav class="navbar navbar-light navbar-expand-md bg-light justify-content-center">
-    <a href="/" class="navbar-brand mr-0">Brand</a> 
-    <button class="navbar-toggler ml-1" type="button" data-toggle="collapse" data-target="#collapsingNavbar2">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="navbar-collapse collapse justify-content-between align-items-center w-100" id="collapsingNavbar2">
-        <ul class="navbar-nav mx-auto text-center">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">Home</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="//codeply.com">News e Eventi</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Chi siamo</a> 
-            </li>
-        </ul>
-        <ul class="nav navbar-nav flex-row justify-content-center flex-nowrap">
-            <li class="nav-item"><a class="nav-link" href=""><i class="fa fa-facebook mr-1"></i></a> </li>
-            <li class="nav-item"><a class="nav-link" href=""><i class="fa fa-twitter"></i></a> </li>
-        </ul>
-    </div>
-</nav>
+        <nav class="navbar fixed-top navbar-light navbar-expand-md bg-light justify-content-center">
+                <button class="navbar-toggler ml-1" type="button" data-toggle="collapse" data-target="#collapsingNavbar2">
+                        <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="navbar-collapse collapse justify-content-between align-items-center w-100" id="collapsingNavbar2">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link ncol" href="#">Home <span class="sr-only">Home</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link ncol" href="//codeply.com">News e Eventi</a>
+                        </li>
+                        <li class="nav-item">
+                                <a class="nav-link ncol" href="//codeply.com">Progetti</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link ncol" href="#">Chi siamo</a> 
+                        </li>
+                        <li class="nav-item">
+                                <a class="nav-link ncol" href="#">Contatti</a> 
+                        </li>
+                    </ul>
+                </div>
+            </nav>
     </header>    
 
     <form action="../php/up_progetti.php" method="POST">
@@ -46,16 +48,16 @@
             </div>
         
             <div class="form-group">
-                <label for="text">Example textarea</label>
+                <label for="text">Contenuto</label>
                 <textarea class="form-control" id="text" rows="3" name="text"></textarea>
             </div>
 
             <div class="form-group">
-                <label for="file">Example file input</label>
+                <label for="file">Inserisci immagine</label>
                 <input type="file" class="form-control-file" id="file" name="file">
             </div>
             <div class="form-group">
-                <label for="tipo">Example select</label>
+                <label for="tipo">Progetto - Evento - News</label>
                 <select class="form-control" id="tipo" name="tipo">
                     <option>Progetto</option>
                     <option>Evento</option>
