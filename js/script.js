90fbaf9659e80aa21b5a8b7f61fa48ec955b00f2
mginetti <matteo.ginetti99@gmail.com>
2019-03-22 13:02:08 +0100

    commit a caso
    viva la casualità

--- js/script.js
+++ js/script.js
@@ -13,13 +13,18 @@ $(window).scroll(function() {
 });
 
 $(window).scroll(function(){
-    parallax();
+  
+        parallax();
+    
   });
   
   function parallax(){
     var scrolled = $(window).scrollTop();
+    
     $('.bg').css('top',-(scrolled*0.1)+'px');
     $('header .content').css('top',50+(scrolled*0.1)+'%');
     $('header .content').css('opacity',1-(scrolled*0.01)/10);
     $('header .content').css('opacity',1-(scrolled*0.01)/10);
+     
+    
   };
