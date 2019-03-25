1ab77c95a3041cb5d70687f2a32749777e77fa2e
Federico Bolcato <2306632+Freedye@users.noreply.github.com>
2019-03-25 11:48:17 +0100

    Navbar fix
    Aggiunto logo alle navbar e una piccola ombreggiatura per separarla dal body

--- pages/news.php
+++ pages/news.php
@@ -20,6 +20,11 @@
   <body>
     <header>
         <nav class="navbar fixed-top navbar-light navbar-expand-md bg-light justify-content-left">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src="../media/img/effe.png" alt="" class="logo">
+                </a>
+            </div>
             <button class="navbar-toggler ml-1" type="button" data-toggle="collapse" data-target="#collapsingNavbar2">
                 <span class="navbar-toggler-icon"></span>
             </button>
