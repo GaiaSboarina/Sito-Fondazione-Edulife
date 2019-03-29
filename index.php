044687c85ab4cc5145cd8fe664f02a1769e281a9
Federico Bolcato <2306632+Freedye@users.noreply.github.com>
2019-03-29 17:49:31 +0100

    Clean up del repository
    Sistemate alcune cosette e cancellate alcune pagine

--- index.php
+++ index.php
@@ -6,13 +6,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <link rel="stylesheet" href="css/style_index.css">
     <link rel="stylesheet" href="css/glider.css">
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/font.css">
-    <link rel="stylesheet" href="../css/style_gxg.css">
+    <link rel="stylesheet" href="css/font.css">
+    <link rel="stylesheet" href="css/style_gxg.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    <link href="css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     
     <title>Fondazione Edulife Onlus</title>
 </head>
