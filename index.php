89e871a660308779f5a33ef02df0d7a9b45fce29
Federico Bolcato <2306632+Freedye@users.noreply.github.com>
2019-03-30 17:00:03 +0100

    Piccolo update all'index

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
         <nav class="navbar fixed-top navbar-light navbar-expand-md bg-light justify-content-left">
             <div class="container">
                 <a class="navbar-brand" href="#">
-                    <img src="../media/img/effe.png" alt="" class="logo">
+                    <img src="media/img/effe.png" alt="" class="logo">
                 </a>
             </div>
             <button class="navbar-toggler ml-1" type="button" data-toggle="collapse" data-target="#collapsingNavbar2">
