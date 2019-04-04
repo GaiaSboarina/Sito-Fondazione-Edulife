f59e37395147838592d89951db03503973760329
Federico Bolcato <2306632+Freedye@users.noreply.github.com>
2019-04-04 22:49:55 +0200

    Adesso va l'immagine nei progetti
    Sono un mona

--- php/insert_progetti.php
+++ php/insert_progetti.php
@@ -13,7 +13,7 @@
         } 
         //query necessaria per evitare che i caratteri non inglesi (tipo le lettere accentate) non vengano visualizzati
         $conn->query("SET NAMES 'utf8'");
-        $query = "SELECT p.titolo, p.contenuto, i.nome FROM progetto p, img i";
+        $query = "SELECT p.titolo, p.contenuto, i.nome FROM progetto p, img i WHERE p.id_img = i.id";
         $result = $conn->query($query);
         if ($result->num_rows > 0) {
             while($row = $result->fetch_assoc()){
@@ -23,7 +23,7 @@
                             </h1>
                             <div class='row'>
                                 <div class='col-md-8'>
-                                    <div><img border='0' src='media/img/".$row['nome']."'></div>
+                                    <div><img border='0' src='../media/img/".$row['nome']."'></div>
                                 </div>
                                 <div class='col-md-4'>
                                     <p>".$row['contenuto']."</p>
