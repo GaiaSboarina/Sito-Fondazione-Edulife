80a3491813d67fe42fb3ba03eb2026b365bc5f9f
Federico Bolcato <2306632+Freedye@users.noreply.github.com>
2019-04-04 23:41:05 +0200

    Aggiunte immagini a news e progetti
    Fixati anche i vari collegamenti al db

--- php/insert_news.php
+++ php/insert_news.php
@@ -1,27 +1,39 @@
 <?php
     function getAllNews(){
-        $connection = mysqli_connect("localhost", "root", "", "edulife");
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "edulife";
+
+        // Creazione della connessione
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Controllo della connessione
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } 
         //query necessaria per evitare che i caratteri non inglesi (tipo le lettere accentate) non vengano visualizzati
-        $connection->query("SET NAMES 'utf8'");
-        $rsNews = mysqli_query($connection, "SELECT * FROM news");
-        $news = mysqli_fetch_all($rsNews, MYSQLI_ASSOC);
-        mysqli_close($connection);
-        for($i=0; $i < count($news); $i++){
-            echo "
-                <div class='container'>
-                    <h1 class='my-4'>".$news[$i]['titolo']."
-                    </h1>
-                    <div class='row'>
-                        <div class='col-md-8'>
-                            <img class='img-fluid' src=".$news[$i]['id_img']." alt=''>
+        $conn->query("SET NAMES 'utf8'");
+        $query = "SELECT n.titolo, n.contenuto, i.nome FROM news n, img i WHERE n.id_img = i.id";
+        $result = $conn->query($query);
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()){
+                    echo "
+                            <div class='container'>
+                            <h1 class='my-4'>".$row['titolo']."
+                            </h1>
+                            <div class='row'>
+                                <div class='col-md-8'>
+                                    <div><img border='0' src='../media/img/".$row['nome']."'></div>
+                                </div>
+                                <div class='col-md-4'>
+                                    <p>".$row['contenuto']."</p>
+                                </div>
+                            </div>
                         </div>
-                        <div class='col-md-4'>
-                            <p>".$news[$i]['contenuto']."</p>
-                        </div>
-                    </div>
-                </div>
-                <br>
-            ";
-        }
-    }
+                        <br>
+                    ";
+                }
+            }
+    mysqli_close($conn);
+}
 ?>
--- php/up_progetti.php
+++ php/up_progetti.php
@@ -10,7 +10,8 @@
 
         // Creazione della connessione
         $conn = new mysqli($servername, $username, $password, $dbname);
-        
+        $conn->query("SET NAMES 'utf8'");
+
         // Controllo della connessione
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
