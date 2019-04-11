f0feeec4f77fe089afe7dacdf64f88dce8b23241
Federico Bolcato <2306632+Freedye@users.noreply.github.com>
2019-04-11 15:05:00 +0200

    Aggiunti collegamenti social per mobile
    E uno

--- index.php
+++ index.php
@@ -70,6 +70,22 @@
                               </li>
                         </ul>
                   </nav>
+                  <nav>
+				<p class="socialMobile" style="color: #e21212;">
+                              <a href="https://www.facebook.com/FondazioneEdulife" target="_blank">
+                                    <i class="fa fa-facebook social_home" aria-hidden="true" style="color: #e21212;"></i>
+                              </a>
+                              <a href="https://www.instagram.com/fondazione.edulife/?hl=it" target="_blank">
+                                    <i class="fa fa-instagram social_home" aria-hidden="true" style="color: #e21212;"></i>
+                              </a>
+                              <a href="https://twitter.com/EdulifeOnlus" target="_blank">
+                                    <i class="fa fa-twitter social_home" aria-hidden="true" style="color: #e21212;"></i>
+                              </a>
+                              <a href="https://www.google.it/maps/place/311+Verona/@45.4303778,10.9963367,17z/data=!3m1!4b1!4m5!3m4!1s0x477f5f3fd94d631f:0xb9a1156ee2f8c5e!8m2!3d45.4303741!4d10.9985254" target="_blank">
+                                    <i class="fa fa-map-marker social_home" aria-hidden="true" style="color: #e21212;"></i>
+                              </a>
+				</p>
+                  </nav>
             </div><!-- /#gloval-nav -->
             
             <div class="social-media">
